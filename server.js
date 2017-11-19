fdd97e77f17f36977307204fbeceeb0b4280c9c8
Eric Laursen <ericjlaursen@gmail.com>
2017-11-19 17:02:41 -0500

    includes CORS in express

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
  const express = require('express');
  const app = express();
+ const cors = require('cors');
+ const {CLIENT_ORIGIN} = require('./config');
 
  const PORT = process.env.PORT || 3000;
+
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
 
  app.get('/api/*', (req, res) => {
    res.json({ok: true});
