f241bd3d1f3e831219be3a624c7f0c6a77d4a462
Eric Laursen <ericjlaursen@gmail.com>
2017-11-22 18:06:44 -0500

    includes basic CORS fnality

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
  const express = require('express');
  const app = express();
  const cors = require('cors');
- const {CLIENT_ORIGIN} = require('./config');
-
+ // const {CLIENT_ORIGIN} = require('./config');
  const PORT = process.env.PORT || 3000;
 
-app.use(
-    cors({
-        origin: CLIENT_ORIGIN
-    })
-);
+// app.use(
+//     cors({
+//         origin: CLIENT_ORIGIN
+//     })
+// );
+
+
+// CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+
 
  app.get('/api/*', (req, res) => {
    res.json({ok: true});
