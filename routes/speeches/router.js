1432de0fba968cbcb7648b415ba785e4eb711e90
Eric Laursen <ericjlaursen@gmail.com>
2017-11-26 11:52:58 -0500

    includes local AND cloud findById for default GET

--- routes/speeches/router.js
+++ routes/speeches/router.js
@@ -11,7 +11,8 @@ router.get('/', (req, res) => {
 //Get Default Trump 2017 Speech, by ID
 router.get('/default', (req,res) => {
 	Stat
-		.findById("5a1ab3a26b7b26dfb8aeade3")
+		.findById("5a1ab3a26b7b26dfb8aeade3")	//LOCAL
+		.findById("5a1ad99f978ca2681f42df12")	//CLOUD
 		.then(stat => res.json(stat.apiRepr()))
 		.catch(err => {
 			console.log(err);
