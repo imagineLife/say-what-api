357724c6ef19af975211cc65683d42167e3a5b16
Eric Laursen <ericjlaursen@gmail.com>
2017-11-26 12:02:24 -0500

    updates url for test

--- test/test-server.js
+++ test/test-server.js
@@ -10,7 +10,7 @@
 
    it('should 200 on GET requests', function() {
      return chai.request(app)
-       .get('/api/fooooo')
+       .get('/api/speech')
        .then(function(res) {
          res.should.have.status(200);
          res.should.be.json;
