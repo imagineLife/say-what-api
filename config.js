11e83749bb41542717814898c56444e85c5437a7
Eric Laursen <ericjlaursen@gmail.com>
2017-11-26 20:21:52 -0500

    re-enable local DB

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      // 'mongodb://localhost/speechs';
-                      'mongodb://speechUser:Speech8User6@ds119476.mlab.com:19476/speechs';
+                      'mongodb://localhost/speechs';
+                      // 'mongodb://speechUser:Speech8User6@ds119476.mlab.com:19476/speechs';
 exports.PORT = process.env.PORT || 8080;
