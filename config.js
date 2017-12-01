0e716399329cb8af2f82d5c398aec677aae923ad
Eric Laursen <ericjlaursen@gmail.com>
2017-11-30 19:28:33 -0500

    includes test DB url

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                       'mongodb://localhost/speechs';
                       // 'mongodb://speechUser:Speech8User6@ds119476.mlab.com:19476/speechs';
+exports.TEST_DATABASE_URL = 'mongodb://localhost/speechs-test';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '5m';
