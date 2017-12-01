0ab6d31cc2608ddad1daf6acdc0ba7be8c804d0c
Eric Laursen <ericjlaursen@gmail.com>
2017-12-01 07:46:24 -0500

    includes placeholder add-request

--- routes/speeches/router.js
+++ routes/speeches/router.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {Stat} = require('./models');
+const passport = require('passport');
+const {router: jwtStrategy} = require('../auth');
 
+passport.use(jwtStrategy);
 
 router.get('/', (req, res) => {
   return res.json({ok: true});
@@ -11,7 +14,7 @@ router.get('/', (req, res) => {
 //Get Default Trump 2017 Speech, by ID
 router.get('/default', (req,res) => {
 	Stat
-		.findById("5a1f441aee30112b4312157d")	//LOCAL
+		.findById("5a1ff02cb56e0347e480296a")	//LOCAL
 		// .findById("5a1ad99f978ca2681f42df12")	//CLOUD
 		.then(stat => res.json(stat.apiRepr()))
 		.catch(err => {
@@ -35,16 +38,58 @@ router.get('/:id', (req, res) => {
 });
 
 
-// //Get TEXT of speech by given speech ID
-// router.get('/text/:id', (req, res) => {
-//   Stat
-//     .findById(req.params.id)
-//     .exec()
-//     .then(stat => res.json(stat.apiRepr()))
-//     .catch(err => {
-//       console.error(err);
-//       res.status(500).json({error: 'something went horribly awry'});
-//     });
-// });
+//Get TEXT of speech by given speech ID
+router.get('/text/:id', (req, res) => {
+  Stat
+    .findById(req.params.id)
+    .select("speechTextLink")
+    .exec()
+    .then(stat =>  fs.readFileSync(stat.speechTextLink, 'utf8'))
+    // .then(stat => res.json(stat.speechTextLink))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({error: 'something went horribly awry'});
+    });
+});
+
+router.post('/request',
+  passport.authenticate('jwt', {session: false, failWithError: true}),
+  (req,res) => {
+    
+//Make-Sure request has necessary fields
+    const requiredFields = ['type', 'text'];
+    for (let i=0; i<requiredFields.length; i++) {
+      const field = requiredFields[i];
+      if (!(field in req.body)) {
+        const message = `Missing \`${field}\` in request body`
+        console.error(message);
+        return res.status(400).send(message);
+      }
+    }
+
+    Request
+      .create({
+        type: req.body.type,
+        text: req.body.text,
+        user: req.user._id
+      })
+      .then(requestMade => {
+        let id = requestMade.user;      
+        User  
+          .findByIdAndUpdate(id,
+          { "$push": { "requestMade": requestMade._id } },
+          {"new" : true},
+          function(err,user){
+            console.log(user);
+          }
+        )
+        return requestMade;
+      })
+      .then(requestMade => {res.status(201).json(requestMade.apiRepr()) })
+      .catch(err => {
+          console.error(err);
+          res.status(500).json({error: 'Something went wrong'});
+      });
+})
 
 module.exports = router;
