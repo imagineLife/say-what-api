6f3efe18724ab8eba7b62bf68a3e39d8420d254e
Eric Laursen <ericjlaursen@gmail.com>
2017-12-03 13:48:06 -0500

    Removes dev request endpoint.

--- routes/speeches/router.js
+++ routes/speeches/router.js
@@ -52,44 +52,4 @@ router.get('/text/:id', (req, res) => {
     });
 });
 
-router.post('/request',
-  passport.authenticate('jwt', {session: false, failWithError: true}),
-  (req,res) => {
-    
-//Make-Sure request has necessary fields
-    const requiredFields = ['type', 'text'];
-    for (let i=0; i<requiredFields.length; i++) {
-      const field = requiredFields[i];
-      if (!(field in req.body)) {
-        const message = `Missing \`${field}\` in request body`
-        console.error(message);
-        return res.status(400).send(message);
-      }
-    }
-
-    Request
-      .create({
-        type: req.body.type,
-        text: req.body.text,
-        user: req.user._id
-      })
-      .then(requestMade => {
-        let id = requestMade.user;      
-        User  
-          .findByIdAndUpdate(id,
-          { "$push": { "requestMade": requestMade._id } },
-          {"new" : true},
-          function(err,user){
-            console.log(user);
-          }
-        )
-        return requestMade;
-      })
-      .then(requestMade => {res.status(201).json(requestMade.apiRepr()) })
-      .catch(err => {
-          console.error(err);
-          res.status(500).json({error: 'Something went wrong'});
-      });
-})
-
 module.exports = router;
