e353ab04618bf11fcf2ad788f6018799d2f96a2a
Eric Laursen <ericjlaursen@gmail.com>
2017-12-03 12:47:44 -0500

    Re-orders imports and removes dev lines.

--- routes/requests/router.js
+++ routes/requests/router.js
@@ -1,68 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const {Request} = require('./models');
 const passport = require('passport');
+
+const {Request} = require('./models');
+const {User} = require('../users/models');
 const {router: jwtStrategy} = require('../auth');
 
 passport.use(jwtStrategy);
 
 
-//Get Request-list By user-ID
+//Get POPULATED Request-list By user-ID
 // A PROTECTED endpoint which needs a valid JWT to access it
-router.get('/api/requests',
+router.get('/',
     passport.authenticate('jwt', {session: false}),
     (req, res) => {
-          Request
+          User
           .findById(req.user._id)
-          .populate({path: 'suggestions'})
+          .populate('requests')
           .exec()
-          .then(user => res.status(201).json(user.suggestions))
+          .then(user => res.status(201).json(user.requests))
           .catch(err => {
             console.error(err);
             res.status(500).json({error: 'something went terribly wrong'});
           });
     }
 );
-
-
-router.post('/request',
-  passport.authenticate('jwt', {session: false, failWithError: true}),
-  (req,res) => {
-    
-//Make-Sure request has necessary fields
-    const requiredFields = ['type', 'text'];
-    for (let i=0; i<requiredFields.length; i++) {
-      const field = requiredFields[i];
-      if (!(field in req.body)) {
-        const message = `Missing \`${field}\` in request body`
-        console.error(message);
-        return res.status(400).send(message);
-      }
-    }
-
-    Request
-      .create({
-        type: req.body.type,
-        text: req.body.text,
-        user: req.user._id
-      })
-      .then(requestMade => {
-        let id = requestMade.user;      
-        User  
-          .findByIdAndUpdate(id,
-          { "$push": { "requestMade": requestMade._id } },
-          {"new" : true},
-          function(err,user){
-            console.log(user);
-          }
-        )
-        return requestMade;
-      })
-      .then(requestMade => {res.status(201).json(requestMade.apiRepr()) })
-      .catch(err => {
-          console.error(err);
-          res.status(500).json({error: 'Something went wrong'});
-      });
-})
 
 module.exports = router;
