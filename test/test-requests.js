3eb0c6fe29e33805779ed6e06360595f0350dd56
Eric Laursen <ericjlaursen@gmail.com>
2017-12-06 19:59:20 -0500

    Removes lots of dev lines.

--- test/test-requests.js
+++ test/test-requests.js
@@ -40,21 +40,27 @@ describe('\nRequest endpoints\n', function () {
   });
 
   beforeEach(function () {
+      
     return User.hashPassword(password).then(password =>
+      
       User.create({
         username,
         password,
         firstName,
         lastName
       })
+    
     );
+
+    console.log('beforeEach', User);
+  
   });
 
   afterEach(function () {
     return User.remove({});
   });
 
-  describe('\nPOST to /api/requests\n', function () {
+  describe('POST to /api/requests\n', function () {
     it('Should reject requests with no credentials: code 401', function () {
       return chai
       .request(app)
@@ -94,7 +100,7 @@ describe('\nRequest endpoints\n', function () {
     //       expect(res.body).to.have.keys('username', 'firstName', 'lastName', 'requests');
     //     });
     // });
-    it('Should send request & return proper response', function() {
+    it('Should return proper status, keys, and type', function() {
       const token = jwt.sign(
         {
           user: {
@@ -120,7 +126,6 @@ describe('\nRequest endpoints\n', function () {
           expect(res).to.have.status(201);
           expect(res.body).to.have.keys('id', 'type', 'text', 'Date');
           expect(res.body).to.be.an('object');
-          // expect(res.body.data).to.equal('rosebud');
         });
     });
     // it('Should reject requests with an invalid token', function() {
@@ -187,124 +192,4 @@ describe('\nRequest endpoints\n', function () {
     //     });
     // });
   });
-
-  // describe('POST to /auth/refresh', function () {
-  //   it('Should reject requests with no credentials', function () {
-  //     return chai
-  //       .request(app)
-  //       .post('/api/auth/refresh')
-  //       .then(() =>
-  //         expect.fail(null, null, 'Request should not succeed')
-  //       )
-  //       .catch(err => {
-  //         if (err instanceof chai.AssertionError) {
-  //           throw err;
-  //         }
-
-  //         const res = err.response;
-  //         expect(res).to.have.status(401);
-  //       });
-  //   });
-  //   it('Should reject requests with an invalid token', function () {
-  //     const token = jwt.sign(
-  //       {
-  //         username,
-  //         firstName,
-  //         lastName
-  //       },
-  //       'wrongSecret',
-  //       {
-  //         algorithm: 'HS256',
-  //         expiresIn: '7d'
-  //       }
-  //     );
-
-  //     return chai
-  //       .request(app)
-  //       .post('/api/auth/refresh')
-  //       .set('Authorization', `Bearer ${token}`)
-  //       .then(() =>
-  //         expect.fail(null, null, 'Request should not succeed')
-  //       )
-  //       .catch(err => {
-  //         if (err instanceof chai.AssertionError) {
-  //           throw err;
-  //         }
-
-  //         const res = err.response;
-  //         expect(res).to.have.status(401);
-  //       });
-  //   });
-  //   it('Should reject requests with an expired token', function () {
-  //     const token = jwt.sign(
-  //       {
-  //         user: {
-  //           username,
-  //           firstName,
-  //           lastName
-  //         },
-  //         exp: Math.floor(Date.now() / 1000) - 10 // Expired ten seconds ago
-  //       },
-  //       JWT_SECRET,
-  //       {
-  //         algorithm: 'HS256',
-  //         subject: username
-  //       }
-  //     );
-
-  //     return chai
-  //       .request(app)
-  //       .post('/api/auth/refresh')
-  //       .set('authorization', `Bearer ${token}`)
-  //       .then(() =>
-  //         expect.fail(null, null, 'Request should not succeed')
-  //       )
-  //       .catch(err => {
-  //         if (err instanceof chai.AssertionError) {
-  //           throw err;
-  //         }
-
-  //         const res = err.response;
-  //         expect(res).to.have.status(401);
-  //       });
-  //   });
-  //   it('Should return a valid auth token with a newer expiry date', function () {
-  //     const token = jwt.sign(
-  //       {
-  //         user: {
-  //           username,
-  //           firstName,
-  //           lastName
-  //         }
-  //       },
-  //       JWT_SECRET,
-  //       {
-  //         algorithm: 'HS256',
-  //         subject: username,
-  //         expiresIn: '7d'
-  //       }
-  //     );
-  //     const decoded = jwt.decode(token);
-
-  //     return chai
-  //       .request(app)
-  //       .post('/api/auth/refresh')
-  //       .set('authorization', `Bearer ${token}`)
-  //       .then(res => {
-  //         expect(res).to.have.status(200);
-  //         expect(res.body).to.be.an('object');
-  //         const token = res.body.authToken;
-  //         expect(token).to.be.a('string');
-  //         const payload = jwt.verify(token, JWT_SECRET, {
-  //           algorithm: ['HS256']
-  //         });
-  //         expect(payload.user).to.deep.equal({
-  //           username,
-  //           firstName,
-  //           lastName
-  //         });
-  //         expect(payload.exp).to.be.at.least(decoded.exp);
-  //       });
-  //   });
-  // });
 });
