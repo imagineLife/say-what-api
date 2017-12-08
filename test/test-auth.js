07a46c2154f1e9ba4abd3c46a948a124d72665ae
Eric Laursen <ericjlaursen@gmail.com>
2017-12-08 06:11:44 -0500

    Removes dev. lines.

--- test/test-auth.js
+++ test/test-auth.js
@@ -198,43 +198,5 @@ describe('Auth endpoints\n', function () {
           expect(res).to.have.status(401);
         });
     });
-    // it('Should return a valid auth token with a newer expiry date', function () {
-    //   const token = jwt.sign(
-    //     {
-    //       user: {
-    //         username,
-    //         firstName,
-    //         lastName
-    //       }
-    //     },
-    //     JWT_SECRET,
-    //     {
-    //       algorithm: 'HS256',
-    //       subject: username,
-    //       expiresIn: '7d'
-    //     }
-    //   );
-    //   const decoded = jwt.decode(token);
-
-    //   return chai
-    //     .request(app)
-    //     .post('/api/auth/refresh')
-    //     .set('authorization', `Bearer ${token}`)
-    //     .then(res => {
-    //       expect(res).to.have.status(200);
-    //       expect(res.body).to.be.an('object');
-    //       const token = res.body.authToken;
-    //       expect(token).to.be.a('string');
-    //       const payload = jwt.verify(token, JWT_SECRET, {
-    //         algorithm: ['HS256']
-    //       });
-    //       expect(payload.user).to.deep.equal({
-    //         username,
-    //         firstName,
-    //         lastName
-    //       });
-    //       expect(payload.exp).to.be.at.least(decoded.exp);
-    //     });
-    // });
   });
 });
