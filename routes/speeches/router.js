c36ae77bd8ea3d1f9c6f44df09367b4a8db26f9d
Eric Laursen <ericjlaursen@gmail.com>
2018-01-08 08:01:53 -0500

    Includes Date in speechList endpoint.

--- routes/speeches/router.js
+++ routes/speeches/router.js
@@ -46,7 +46,7 @@ router.get('/speechList',
   passport.authenticate('jwt', { session: false }),
  (req,res) => {
   Stat
-    .find().select('_id title Orator') //LOCAL
+    .find().select('_id title Orator Date') //LOCAL
     .then(stat => res.json(stat))
     .catch(err => {
       console.log(err);
