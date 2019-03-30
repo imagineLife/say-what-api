9f656677caafeda44b390f9a8df85e2fa94729a7
Eric Laursen <ericjlaursen@gmail.com>
2019-03-30 12:26:25 -0400

    removes extraneous speechstats from the model

--- routes/speeches/models.js
+++ routes/speeches/models.js
@@ -15,27 +15,6 @@ const speechSchema = mongoose.Schema({
 		type: Date,
 		required: true
 	},
-	Audience : {
-		type: String,
-		required: true
-	},
-	numberOfWords : {
-		wordCount : Number,
-		uniqueWords : Number
-	},
-	mostUsedWords : [
-		{
-			word : String,
-			occurances : Number
-		}
-	],
-	wordsBySize : [
-		{
-			size : Number,
-			occurances : Number
-		}
-	],
-	bigWords : [String],
 	speechTextLink: {
 		type: String
 	},
@@ -53,11 +32,6 @@ speechSchema.methods.apiRepr = function(){
 		title: this.title,
 		Orator: this.Orator,
 		Date: this.Date,
-		Audience: this.Audience,
-		numberOfWords: this.numberOfWords,
-		mostUsedWords: this.mostUsedWords,
-		wordsBySize: this.wordsBySize,
-		bigWords: this.bigWords,
 		speechTextLink: this.speechTextLink,
 		imageLink: this.imageLink,
 		eventOverview: this.eventOverview
