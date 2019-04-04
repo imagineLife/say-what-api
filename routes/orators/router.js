dc8d06d22a42ac40252410f76082727c835394ad
Eric Laursen <ericjlaursen@gmail.com>
2019-04-03 21:54:07 -0400

    dummy placeholder for orators. post

--- routes/orators/router.js
+++ routes/orators/router.js
@@ -23,4 +23,20 @@ router.get('/',
     // });
 });
 
+//Get Default
+router.post('/', 
+  (req, res) => {
+    console.log('POST orators dummy endpoint')
+    res.status(200).send('POST orators dummy endpoint Valid request here!')
+    
+    //from old default speech get
+    // Stat
+    // .findById("5a1ad99f978ca2681f42df12")
+    // .then(stat => res.status(200).json(stat.apiRepr()))
+    // .catch(err => {
+    //   console.log(err);
+    //   res.status(500).json({message: 'Handwritten Error :/'})
+    // });
+});
+
 module.exports = router;
