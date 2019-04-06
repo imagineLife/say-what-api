81d0e731aaf26196f559722d7b1ea0139163a441
Eric Laursen <ericjlaursen@gmail.com>
2019-04-06 07:43:41 -0400

    uses txtToArr in getWordCount

--- stats.js
+++ stats.js
@@ -1,7 +1,9 @@
+function txtToArr(txt){ return txt.replace(/(\\n(\\n)?)/g," ").split(" ") }
+
 function getWordCount(txt){
     //'whatever' and 'whatever.'
     //replace removes "\n"s
-    return txt.replace(/(\\n(\\n)?)/g," ").split(" ").length;
+    return txtToArr(txt).length;
 }
 
 module.exports = { getWordCount }
