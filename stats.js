0958c3ef44f722170d9322dccb9869295356a5af
Eric Laursen <ericjlaursen@gmail.com>
2019-04-11 22:17:00 -0400

    placeholder freqUsedWords

--- stats.js
+++ stats.js
@@ -6,4 +6,17 @@ function getWordCount(txt){
     return txtToArr(txt).length;
 }
 
+// creates an object of the USEFUL words occuring most freuqntly
+function frequentUsedWords(wordarr) {
+  var freqUsedWords = {};
+  wordarr.forEach(function(singleWord){
+      if (freqUsedWords.hasOwnProperty(singleWord)) {
+        freqUsedWords[singleWord] +=1
+      } else {
+        freqUsedWords[singleWord] = 1
+      }
+  });
+  return freqUsedWords;
+}
+
 module.exports = { getWordCount }
