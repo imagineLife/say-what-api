e3c6078792ef7382ef864555766ac177a12d32b4
Eric Laursen <ericjlaursen@gmail.com>
2019-05-19 13:23:02 -0400

    removes dev lines

--- test/test-stats.js
+++ test/test-stats.js
@@ -17,23 +17,6 @@ chai.use(chaiHttp);
 
 describe('\nSpeech/Stat Request endpoints\n', function () {
 
-//ask Jack perhaps?
-
-  // const demoUser = {
-  //   "_id" : new mongoose.mongo.ObjectId('5a1ff02cb56e0347e480296a'),
-  //   "username" : "biggieSmalls",
-  //   "password" : "$2a$10$O4zYhxYd/9aKUfQg6M9y2e48kXA/W3Tu24TePZ/9NdqCvtLbnm76S",
-  //   "firstName" : "Notorious",
-  //   "lastName" : "Wallace",
-  //   "email" : "mretfaster@gmail.com",
-  //   "requests" : [
-  //     "5a22a4e4c8d9e8a18e8f1d1a",
-  //     "5a22a4e4c8d9e8a18e8f1d1b",
-  //     "5a22a4e4c8d9e8a18e8f1d1c",
-  //     "5a22a4e4c8d9e8a18e8f1d1d"
-  //   ]
-  // };
-
   const demoSpeechStats = {
     "_id" : new mongoose.mongo.ObjectId('5a1ad99f978ca2681f42df12'),
     "title" : "Trump 2017 Inaugural Address",
@@ -148,31 +131,7 @@ describe('\nSpeech/Stat Request endpoints\n', function () {
   });
 
   describe('GET from /api/speeches/:id', function () {
- 
-//ask Jack perhaps?
 
-    // it('should return speech stats by url-given speechID', function() {
-    //   // strategy:
-    //   //    1. get back all stats returned by by GET request to `/stats`
-    //   //    2. prove res has right status, data type
-    //   //    3. prove the number of stats we got back is equal to number
-    //   //       in db.
-    //   // need to have access to mutate and access `res` across
-    //   // `.then()` calls below, so declare it here so can modify in place
-    //   let res;
-    //   return chai.request(app)
-    //     .get('/api/speeches/5a1ad99f978ca2681f42df12')
-    //     .set('Authorization', `Bearer ${jwToken}`)
-    //     .then(function(_res) {
-    //       // so subsequent .then blocks can access resp obj.
-    //       res = _res;
-    //       // res.should.have.status(200);
-    //       // otherwise our db seeding didn't work
-    //       // res.body.quickstats.should.have.length.of.at.least(1);
-    //       expect(res.body).to.contain.keys('title', 'Orator');
-    //       return Stat.count();
-    //     })
-    // });
     it('expects status, specific type, and specific id', function() {
       const token = jwt.sign(
         {
@@ -201,19 +160,19 @@ describe('\nSpeech/Stat Request endpoints\n', function () {
         });
     });
   });
-  describe('GET from /api/speeches/default', function () {
-    it('expects status, type==, id==, keyMatching', function() {
-      return chai
-        .request(app)
-        .get('/api/speeches/default')
-        .then(res => {
-          expect(res).to.have.status(200);
-          expect(res.body).to.be.an('object');
-          expect(res.body.id).to.equal('5a1ad99f978ca2681f42df12');     //mLab
-          expect(res.body).to.have.keys('id', 'title', 'Orator', 'Date', 'Audience', 'numberOfWords', 'mostUsedWords', 'wordsBySize', 'bigWords', 'speechTextLink')
-        });
-    });
-  });
+  // describe('GET from /api/speeches/default', function () {
+  //   it('expects status, type==, id==, keyMatching', function() {
+  //     return chai
+  //       .request(app)
+  //       .get('/api/speeches/default')
+  //       .then(res => {
+  //         expect(res).to.have.status(200);
+  //         expect(res.body).to.be.an('object');
+  //         expect(res.body.id).to.equal('5a1ad99f978ca2681f42df12');     //mLab
+  //         expect(res.body).to.have.keys('id', 'title', 'Orator', 'Date', 'Audience', 'numberOfWords', 'mostUsedWords', 'wordsBySize', 'bigWords', 'speechTextLink')
+  //       });
+  //   });
+  // });
   describe('GET from /api/speeches/text/5a1ad99f978ca2681f42df12', function () {
     it('expects status, type', function() {
        const token = jwt.sign(
