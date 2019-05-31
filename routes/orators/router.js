58fe8f693cf9079c1855b1e49c7a5fcaf5ccb1a0
Eric Laursen <ericjlaursen@gmail.com>
2019-05-31 16:00:44 -0400

    simple orators get

--- routes/orators/router.js
+++ routes/orators/router.js
@@ -1,42 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {Orator} = require('./models');
-const passport = require('passport');
-const {router: jwtStrategy} = require('../auth');
-const fs = require('fs');
-var path = require('path');
-passport.use(jwtStrategy);
+const Orator = require('./models');
 
 //Get Default
-router.get('/', 
-  (req, res) => {
-    console.log('GET orators here')
-    res.status(200).send('Valid request here!')
-    
-    //from old default speech get
-    // Stat
-    // .findById("5a1ad99f978ca2681f42df12")
-    // .then(stat => res.status(200).json(stat.apiRepr()))
-    // .catch(err => {
-    //   console.log(err);
-    //   res.status(500).json({message: 'Handwritten Error :/'})
-    // });
-});
-
-//Get Default
-router.post('/', 
-  (req, res) => {
-    console.log('POST orators dummy endpoint')
-    res.status(200).send('POST orators dummy endpoint Valid request here!')
-    
-    //from old default speech get
-    // Stat
-    // .findById("5a1ad99f978ca2681f42df12")
-    // .then(stat => res.status(200).json(stat.apiRepr()))
-    // .catch(err => {
-    //   console.log(err);
-    //   res.status(500).json({message: 'Handwritten Error :/'})
-    // });
+router.get('/', (req, res) => {
+    Orator.find().exec().then(orRes => {
+        res.json(orRes)
+    });    
 });
 
 module.exports = router;
