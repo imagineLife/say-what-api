e2010cf499926f193d408be444deb89172aaa7a0
Eric Laursen <ericjlaursen@gmail.com>
2022-05-23 07:38:45 -0400

    lint

--- index.js
+++ index.js
@@ -1,50 +1,46 @@
 //Dependencies
 const { twoAreEqual } = require('./helpers')
-const { 
-  expressObj, 
-  startServer,
-  setupDB
-} = require('./server-setup')
+const { expressObj, startServer, setupDB } = require('./server-setup')
 const { Crud } = require('./models')
 const { ServicesEmitter } = require('./global')
-const { db: {
-  NAME: DB_NAME,
-  collections: {
-    USERS
-  }
-} } = require('./global/constants')
+const {
+  db: {
+    NAME: DB_NAME,
+    collections: { USERS },
+  },
+} = require('./global/constants')
 let globalContent = require('./global')
-async function startApi(){
-  try{
-
+async function startApi() {
+  try {
     if (!process.env.DB || process.env.DB === true) {
       const db_obj = {
         username: process.env.MONGO_DB_USER,
         pw: process.env.MONGO_DB_PW,
         host: process.env.MONGO_DB_HOST,
         port: process.env.MONGO_DB_PORT,
-        authDB: process.env.MONGO_DB_AUTH_DB
+        authDB: process.env.MONGO_DB_AUTH_DB,
       }
       let SayWhatMongoClient = await setupDB({ ...db_obj })
-      let sayWhatDB = SayWhatMongoClient.registerDB(process.env.DB_NAME || DB_NAME)
+      let sayWhatDB = SayWhatMongoClient.registerDB(
+        process.env.DB_NAME || DB_NAME
+      )
 
       /* 
         Register Collections 
         - Users
       */
-      
+
       let UsersCollection = new Crud({
         db: sayWhatDB,
-        collection: USERS
+        collection: USERS,
       })
-      
-      globalContent.GLOBAL_STATE.Collections.Users = UsersCollection;
-      
+
+      globalContent.GLOBAL_STATE.Collections.Users = UsersCollection
     }
 
     startServer(expressObj)
-  }catch(e){
-    console.log(e);
+  } catch (e) {
+    console.log(e)
   }
 }
 
@@ -54,5 +50,5 @@ if (twoAreEqual(require.main, module)) {
 
 module.exports = {
   startServer,
-  startApi
-};
+  startApi,
+}
