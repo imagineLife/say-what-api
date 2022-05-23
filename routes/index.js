caa4b330cd575b560ba176d8d3d208c6ad2b729f
Eric Laursen <ericjlaursen@gmail.com>
2022-05-23 07:41:45 -0400

    format

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,16 @@
-
-const router = require('express').Router()
+const router = require('express').Router();
 const {
-  routes: { 
-    DB,
-    HEALTH_CHECK,
-    SPEECHES,
-    USERS
-  }
+  routes: { DB, HEALTH_CHECK, SPEECHES, USERS },
 } = require('./../global/constants');
-const healthCheckHandler =  require('./healthcheck');
+const healthCheckHandler = require('./healthcheck');
 const dbStatusHandler = require('./dbStatus');
-const speechesHandler = require('./speeches')
-const usersHandler = require('./users')
+const speechesHandler = require('./speeches');
+const usersHandler = require('./users');
 
-router.use(DB.ROOT, dbStatusHandler)
-router.use(HEALTH_CHECK, healthCheckHandler)
-router.use(SPEECHES.ROOT, speechesHandler)
-router.use(USERS.ROOT, usersHandler)
+router.use(DB.ROOT, dbStatusHandler);
+router.use(HEALTH_CHECK, healthCheckHandler);
+router.use(SPEECHES.ROOT, speechesHandler);
+router.use(USERS.ROOT, usersHandler);
 /*
   /users
     get       - get list  
@@ -33,6 +27,6 @@ router.use(USERS.ROOT, usersHandler)
 
 
 
-*/ 
+*/
 
 module.exports = router;
