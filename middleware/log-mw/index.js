a60ab2285cd81e54becf4756f6f4e190bfafc617
Eric Laursen <ericjlaursen@gmail.com>
2022-05-25 19:29:54 -0400

    more log content

--- middleware/log-mw/index.js
+++ middleware/log-mw/index.js
@@ -1,9 +1,11 @@
 const { ServicesEmitter } = require("../../global/events");
-const { LOG_USER_DEETS } = require('../../global/constants');
+const { events: { LOG_USER_DEETS } } = require('../../global/constants');
 
-module.exports = function logMW(req, res, nxt) {  
+module.exports = function logMW(req, res, nxt) { 
   const logObj = {
-    ip: req?.headers? req.headers['x-forwarded-for'] : req.socket.remoteAddress
+    ip: req.socket.remoteAddress,
+    date: new Date().toUTCString(),
+    path: req.path
   };
   ServicesEmitter.emit(LOG_USER_DEETS, logObj);
   nxt()
