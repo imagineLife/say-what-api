5f98a49cde509261d6a9ff748a1b36816aa716fa
Eric Laursen <ericjlaursen@gmail.com>
2022-05-25 19:30:02 -0400

    adjust import syntax

--- middleware/index.js
+++ middleware/index.js
@@ -1,7 +1,7 @@
 const { assertParams } = require('./assertParams');
 const { auth, adminAuth } = require('./auth');
 const { checkForDbConnection } = require('./check-for-db');
-const { logMW } = require('./log-mw');
+const logMW = require('./log-mw');
 
 module.exports = {
   adminAuth,
