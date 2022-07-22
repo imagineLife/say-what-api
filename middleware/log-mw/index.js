1cb6c85d3395ef15249359dcf82f9283425e18b0
Eric Laursen <ericjlaursen@gmail.com>
2022-07-22 09:42:08 -0400

    docs(log-user-deets): comments describing the intent of the file
    comments

--- middleware/log-mw/index.js
+++ middleware/log-mw/index.js
@@ -1,6 +1,12 @@
 const { ServicesEmitter } = require("../../global/events");
 const { events: { LOG_USER_DEETS } } = require('../../global/constants');
 
+/*
+  middleware intended to log "user details" by passing details to the global service emitter
+  - the requester url
+  - the date
+  - the url path
+*/ 
 module.exports = function logMW(req, res, nxt) { 
   const logObj = {
     ip: req.socket.remoteAddress,
