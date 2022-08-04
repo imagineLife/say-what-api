4aaf57947abdf6b8e5bf55b3d9034bd323b888d5
Eric Laursen <ericjlaursen@gmail.com>
2022-08-03 21:35:18 -0400

    chore(inc): more words
    code

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+// dependencies
 const router = require('express').Router();
 const {
   routes: { DB, HEALTH_CHECK, SPEECHES, USERS },
@@ -7,26 +8,28 @@ const dbStatusHandler = require('./dbStatus');
 const speechesHandler = require('./speeches');
 const usersHandler = require('./users');
 
-router.use(DB.ROOT, dbStatusHandler);
-router.use(HEALTH_CHECK, healthCheckHandler);
-router.use(SPEECHES.ROOT, speechesHandler);
-router.use(USERS.ROOT, usersHandler);
-/*
-  /users
-    get       - get list  
-    post      - create one
 
-    /:id
-      get     - get info  re:user
-      patch   - update    re:user
-    
-      /auth
-        post  - first "create account"
-        patch - user req.params
-          ?field - update/set field
+const routesArr = [
+  {
+    path: DB.ROOT,
+    handler: dbStatusHandler,
+  },
+  {
+    path: HEALTH_CHECK,
+    handler: healthCheckHandler,
+  },
+  {
+    path: SPEECHES.ROOT,
+    handler: speechesHandler,
+  },
+  {
+    path: USERS.ROOT,
+    handler: usersHandler,
+  },
+];
 
-
-
-*/
+for (let i = 0; i < routesArr.length; i += 1) { 
+  router.use(routesArr[i].path, routesArr[i].handler)
+}
 
 module.exports = router;
--- routes/users/index.js
+++ routes/users/index.js
@@ -9,6 +9,22 @@ const rootRouter = require('./root');
 const byIdRouter = require('./byId');
 
 /*
+  "/users" route summary
+
+    get       - get list  
+    post      - create one
+
+    "/:id" summary
+      get     - get info  re:user
+      patch   - update    re:user
+    
+      /auth
+        post  - first "create account"
+        patch - user req.params
+          ?field - update/set field
+*/
+
+/*
   register handlers
 */
 router.use(USERS.BY_ID, byIdRouter);
